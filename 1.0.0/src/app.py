2ac262abe05de54b66db54a2d3c04aa21b81260c
xenofonstam <35816520+xenofonstam@users.noreply.github.com>
2021-02-10 00:13:38 +0100

    Update app.py

--- 1.0.0/src/app.py
+++ 1.0.0/src/app.py
@@ -5,7 +5,6 @@ import random
 import json
 from template.py import template as jp 
 from walkoff_app_sdk.app_base import AppBase
-import templates
 
 class PythonPlayground(AppBase):
     __version__ = "1.0.0"
