ccbd73232676cf41019fb9175c7d920ac29e785c
xenofonstam <35816520+xenofonstam@users.noreply.github.com>
2021-02-09 21:24:16 +0100

    Update templates.py

--- 1.0.0/src/templates.py
+++ 1.0.0/src/templates.py
@@ -1,10 +1,21 @@
+from app.py import json_data as jp
+
 image="![enter image description here](https://secureops1.com/wp-content/uploads/2018/06/logo-2.png)"
 
-Test_Rule={"logo":image,"template":""" On {0} the alert Test_Rule was triggered after the {1} request
-was logged towards {2} with return code {3}
-
+switcher = {
+    "Brute-force": image+"""\n\n\nOn {0} the alert {1} was triggered after the {2} request was logged towards {3} with return code {4}\n\n\n
 Event Details:
 {0}
 {1}
 {2}
-{3}""".format(*Test_Rule["arg"])}
+{3}""".format(jp.get("timestamp"),jp.get("rule_name"),jp.get("verb"),jp.get("referrer"),jp.get("response"))),
+  
+    "Port-scan": image+"""\n\n\nOn {0} the alert {1} was triggered after the IP {2} generated traffic towards the destination {3} in {4} unique ports\n\n\n
+Event Details:
+{0}
+{1}
+{2}
+{3}""".format(jp.get("timestamp"),jp.get("rule_name"),jp.get("src_ip"),jp.get("dst_ip"),jp.get("count"))),
+    }
+
+
