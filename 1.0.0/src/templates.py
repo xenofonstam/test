0c8b05d2c40cfe9e5e8428eda3305da797a62a06
xenofonstam <35816520+xenofonstam@users.noreply.github.com>
2021-02-09 21:26:39 +0100

    Update templates.py

--- 1.0.0/src/templates.py
+++ 1.0.0/src/templates.py
@@ -1,7 +1,7 @@
 from app.py import json_data as jp
 
 image="![enter image description here](https://secureops1.com/wp-content/uploads/2018/06/logo-2.png)"
-
+template= ""
 switcher = {
     "Brute-force": image+"""\n\n\nOn {0} the alert {1} was triggered after the {2} request was logged towards {3} with return code {4}\n\n\n
 Event Details:
@@ -18,4 +18,6 @@ Event Details:
 {3}""".format(jp.get("timestamp"),jp.get("rule_name"),jp.get("src_ip"),jp.get("dst_ip"),jp.get("count"))),
     }
 
+template=switcher.get(jp["rule_name"],"No template found")
 
+return template
