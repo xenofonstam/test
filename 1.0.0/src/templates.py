3fb6aa018b7db091ab5f2fccc4615e8648f5fafa
xenofonstam <35816520+xenofonstam@users.noreply.github.com>
2021-02-10 12:54:57 +0100

    Update templates.py

--- 1.0.0/src/templates.py
+++ 1.0.0/src/templates.py
@@ -7,14 +7,14 @@ def apply_template(jp):
     image="![enter image description here](https://secureops.com/wp-content/uploads/2018/06/logo-2.png)"
     template= ""
     switcher = {
-        "Brute-force": image+"""\n\n\nOn {0} the alert {1} was triggered after the {2} request was logged towards {3} with return code {4}\n\n\n
+        "Malicious-Get-Requet": image+"""\n\n\nOn {0} the alert {1} was triggered after the {2} request was logged towards {3} with return code {4}\n\n\n
         Event Details:
         time:{0}
         Rule Name: {1}
         Method: {2}
         Request: {3}""".format(jp.get("timestamp"),jp.get("rule_name"),jp.get("verb"),jp.get("referrer"),jp.get("response")),
 
-        "Port-scan": image+"""\n\n\nOn {0} the alert {1} was triggered after the IP {2} generated traffic towards the destination {3} in {4} unique ports\n\n\n
+        "Brute-force": image+"""\n\n\nOn {0} the alert {1} was triggered after the IP {2} generated traffic towards the destination {3} in {4} unique ports\n\n\n
         Event Details:
         time:{0}
         Rule Name: {1}
